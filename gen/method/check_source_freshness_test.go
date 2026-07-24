d255226e791c84a593d045f477f8b799e33195c7
Travis Cline <travis.cline@gmail.com>
2026-07-24 10:03:06 -0700

    proto: preserve source freshness false

--- gen/method/check_source_freshness_test.go
+++ gen/method/check_source_freshness_test.go
@@ -5,6 +5,7 @@ import (
 	"testing"
 
 	pb "github.com/tmc/nlm/gen/notebooklm/v1alpha1"
+	"github.com/tmc/nlm/internal/beprotojson"
 )
 
 func TestEncodeCheckSourceFreshnessArgs(t *testing.T) {
@@ -23,5 +24,20 @@ func TestEncodeCheckSourceFreshnessArgs(t *testing.T) {
 	const want = `[null,["source-id"],[2,null,[1],[1,null,null,null,null,null,null,null,null,null,[1,3]]]]`
 	if string(got) != want {
 		t.Fatalf("EncodeCheckSourceFreshnessArgs() = %s, want %s", got, want)
+	}
+}
+
+func TestCheckSourceFreshnessFalseResponseRoundTrip(t *testing.T) {
+	const wire = `[null,false,["source-id"]]`
+	var response pb.CheckSourceFreshnessResponse
+	if err := beprotojson.Unmarshal([]byte(wire), &response); err != nil {
+		t.Fatalf("unmarshal: %v", err)
+	}
+	got, err := beprotojson.Marshal(&response)
+	if err != nil {
+		t.Fatalf("marshal: %v", err)
+	}
+	if string(got) != wire {
+		t.Fatalf("round trip = %s, want %s", got, wire)
 	}
 }
